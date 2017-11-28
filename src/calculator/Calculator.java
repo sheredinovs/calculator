8696f6066cd838d05e6c00198bef043d4a662d80
sheredinovs <k.sherfedinov@gmail.com>
2017-11-28 22:30:45 +0200

    added base math operators

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -4,5 +4,21 @@ package calculator;
  * Created by kadyr on 28.11.2017.
  */
 public class Calculator {
-    
+
+    public int plus(int a, int b){
+        return a+b;
+    }
+
+    public int minus(int a, int b){
+        return a-b;
+    }
+
+    public int multiple(int a, int b){
+        return a*b;
+    }
+
+    public int divide(int a, int b){
+        return a/b;
+    }
+
 }
